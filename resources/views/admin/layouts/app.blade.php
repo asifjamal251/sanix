8a4a48d869f8c637a7425bdc980cd61e61be24dc
asif-sanix <asif.sanix@gmail.com>
2018-05-24 16:14:57 +0530

    sanix.in start

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@include('admin.layouts.head')
+</head>
+<body class="hold-transition skin-purple sidebar-mini">
+<div class="wrapper">
+@include('admin.layouts.header')
+@include('admin.layouts.footer')
+</div>
+</body>
+</html>
